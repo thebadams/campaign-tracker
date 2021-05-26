bebea77bbe2d8ae5e703f854265e9608d7704e53
Brian Adams <bpeteradams@gmail.com>
2021-05-26 19:32:24 -0400

    wrote a get user by id route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -4,7 +4,19 @@ const { User } = require('../../models');
 router.post('/', async (req, res) => {
   try {
     const newUser = await User.create(req.body);
-    res.status(200).json(newUser)
+    res.status(200).json(newUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    if (!user) {
+      res.status(400).json({ message: 'No User With That ID Found' });
+    }
+    res.status(200).json(user);
   } catch (error) {
     res.status(500).json(error);
   }
